5ef99c23528ce589cb6fcc624aa09936b082a31b
yjc <756319278@qq.com>
2018-04-23 18:33:23 +0800

    add renderVModel

--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,9 @@
 const addRoutes = () => import('@/views/addRoutes/addRoutes')
+const renderVModel = () => import('@/views/render/renderVModel')
 
 let routerList = [
-  { path: '/addRoutes', component: addRoutes, name: 'addRoutes', description: '使用addRouters动态添加路由，常用于权限分配！' }
+  { path: '/addRoutes', component: addRoutes, name: 'addRoutes', description: '使用addRouters动态添加路由，常用于权限分配！' },
+  { path: '/renderVModel', component: renderVModel, name: 'renderVModel', description: '使用render函数实现v-model！' }
 ]
 
 export default routerList
